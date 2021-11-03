5ccfa7e60e0d838e2f784d451d0103c657fc8740
W Ammerlaan <302446365@student.rocmondriaan.nl>
2021-11-03 14:33:58 +0100

    puzzle 2 bijna klaar

---
+++ js/complete.js
@@ -0,0 +1,8 @@
+const startBTN = document.querySelector(".start-btn");
+
+function startgame() {
+  console.log("test2");
+  window.location.href = "raadsel.html";
+}
+
+startBTN.addEventListener("click", startgame);
---
+++ js/lift.js
@@ -0,0 +1,11 @@
+const input = document.querySelector(".input-veld");
+const nextbtn = document.querySelector(".nextbtn");
+
+function answer() {
+  if (input.value == 3425) {
+    window.location.href = "";
+  } else {
+  }
+}
+
+nextbtn.addEventListener("click", answer);
