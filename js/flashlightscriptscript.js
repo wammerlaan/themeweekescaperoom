832e904e3726719718882cc0c64bca9a5e71cb9e
W Ammerlaan <302446365@student.rocmondriaan.nl>
2021-11-03 12:45:51 +0100

    fix homedesign

---
+++ js/flashlightscriptscript.js
@@ -0,0 +1,20 @@
+function update(e) {
+  let x = e.clientX || e.touches[0].clientX;
+  let y = e.clientY || e.touches[0].clientY;
+
+  document.documentElement.style.setProperty("--cursorX", x + "px");
+  document.documentElement.style.setProperty("--cursorY", y + "px");
+}
+
+document.addEventListener("mousemove", update);
+document.addEventListener("touchmove", update);
+
+const key = document.querySelector(".key");
+
+function keyFound() {
+  console.log("test");
+  alert("nice, je hebt de key gevonden");
+  window.location.href = "index.html";
+}
+
+key.addEventListener("click", keyFound);
