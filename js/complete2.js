837005779a657d9132f7a35cd7aea5b69f557b13
W Ammerlaan <302446365@student.rocmondriaan.nl>
2021-11-04 13:51:12 +0100

    make puzzle 3

---
+++ js/complete2.js
@@ -0,0 +1,8 @@
+const startBTN = document.querySelector(".start-btn");
+
+function startgame() {
+  console.log("test2");
+  window.location.href = "";
+}
+
+startBTN.addEventListener("click", startgame);
--- js/lift.js
+++ js/lift.js
@@ -3,7 +3,7 @@ const nextbtn = document.querySelector(".nextbtn");
 
 function answer() {
   if (input.value == 3425) {
-    window.location.href = "puzzlecomplete2.html";
+    window.location.href = "liftontsnapt.html";
   } else {
     alert("helaas, de code is onjuist");
   }
